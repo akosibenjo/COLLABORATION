ac43c7ffba4dac52e2e41ef4abf83e6d3d737a95
akosibenjo <160323861+akosibenjo@users.noreply.github.com>
2024-03-22 18:39:48 +0800

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,9 @@
+const playboard = document.querySelector(".play-board");
+
+let foodX = 13, foodY = 10; 
+
+const initGame =  () => {
+    let htmlMarkup = `<div class="food"</div>`
+}
+        
+initGame();
